61d827e2ae9f6ba6b78b0a0db7b826a640b81c65
singhet <singhet@SCSWL4072>
2024-02-09 07:44:47 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string sentence = "Sekwencja kwasów nukleinowych: 5-AATTGGCC-3";
+        const string file = "biodata.txt";
+        File.WriteAllText(file, sentence);//saving to a file
+
+        using (var fileStream= new FileStream(file, FileMode.Open))
+        {
+            int sign = sentence.IndexOf("5");
+            fileStream.Seek(sign+1, SeekOrigin.Begin);
+            using (var reader = new StreamReader(fileStream))
+            {
+                string remainingContent = reader.ReadToEnd();
+                Console.WriteLine($"{remainingContent}");
+                Console.ReadKey();
+            }
+        }
+        Console.Clear();
+        File.Delete(file);
+        Console.ReadKey();
+    }
+}
